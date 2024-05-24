e39cb5f86c57982b1e6d00d34f52122d487caa85
Arsen Utepbergenov <arsen.utepbergenov@altenar.com>
2024-05-24 16:09:29 +0300

    /deploy

--- main.js
+++
@@ -1,6 +0,0 @@
-import './src/styles.scss'
-import Menu from './src/menu.js'
-import ShortLink from './src/shortLink.js'
-
-const shortLink = new ShortLink()
-const menu = new Menu()
--- src/menu.js
+++
@@ -1,9 +0,0 @@
-export default class Menu {
-  constructor() {
-    document
-      .getElementById('navMenuButton')
-      .addEventListener('click', () => {
-        document.getElementById('navMobile').classList.toggle('show')
-      })
-  }
-}
